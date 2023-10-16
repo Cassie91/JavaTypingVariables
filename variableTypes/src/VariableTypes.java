0366d2aeb4ccd4294b49e33eb51ee27841d28356
Alessandra da Costa Vieira <alessandracv.contato@gmail.com>
2023-10-16 10:37:41 -0300

    Class: Some examples about variable types.

--- variableTypes/src/VariableTypes.java
+++ variableTypes/src/VariableTypes.java
@@ -1,5 +1,10 @@
 public class VariableTypes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte favoriteNumber = 12;
+        short year = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salary = 1275.33;
     }
 }
